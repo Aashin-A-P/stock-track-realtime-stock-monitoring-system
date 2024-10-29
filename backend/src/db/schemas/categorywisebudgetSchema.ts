db720c6c0845108bc0eec39a24b09ace7eb3fc67
sandhipsuriya <sandhipks@gmail.com>
2024-10-29 14:39:28 +0530

    categorywisebudget schema updTED

--- backend/src/db/schemas/categorywisebudgetSchema.ts
+++ backend/src/db/schemas/categorywisebudgetSchema.ts
@@ -3,9 +3,9 @@ import { budgetsTable } from './budgetsSchema';
 import { categoriesTable } from './categoriesSchema';
 
 export const categoryWiseBudgetsTable = pgTable('category_wise_budgets', {
-    budget_id: integer('budget_id').notNull().references(() => budgetsTable.id, { onDelete: 'cascade' }),
+    budget_id: integer('budget_id').notNull().references(() => budgetsTable.budget_id, { onDelete: 'cascade' }),
     category_id: integer('category_id').notNull().references(() => categoriesTable.category_id, { onDelete: 'cascade' }),
     amount: doublePrecision('amount').notNull(),
   }, (table) => ({
-    pk: primaryKey(budgetsTable.id, categoriesTable.category_id),
-  }))
+    pk: primaryKey({ columns: [table.budget_id, table.category_id] }),
+  }))
