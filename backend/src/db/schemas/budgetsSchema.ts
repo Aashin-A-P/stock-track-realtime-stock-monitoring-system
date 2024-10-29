9bf48be035a59d20ba3e383f553cdfd178c9c6b6
sandhipsuriya <sandhipks@gmail.com>
2024-10-29 14:38:09 +0530

    budget schema updTED

--- backend/src/db/schemas/budgetsSchema.ts
+++ backend/src/db/schemas/budgetsSchema.ts
@@ -1,7 +1,7 @@
 import { date,integer, pgTable } from "drizzle-orm/pg-core";
 
 export const budgetsTable = pgTable("budgets", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  budget_id: integer().primaryKey().generatedAlwaysAsIdentity(),
   startDate: date("start_date").notNull(),
   endDate: date("end_date").notNull(),
   amount: integer("amount").notNull(),
