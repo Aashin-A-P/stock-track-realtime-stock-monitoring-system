829cf136b2a6097c50bb528f316cc881532386b6
John Prabu A <johnprabu0702@gmail.com>
2024-10-30 15:01:31 +0530

    Budgets sample route removed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
 import express, { json, urlencoded, Router } from "express";
-import budgets from "./data/budgets.json";
 
-import budgetsRouter from "./routes/budgetsRoute";
 
 const app = express();
 const port = process.env.PORT;
@@ -12,8 +10,6 @@ app.use(urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Welcome To Stock Management MIT IT");
 });
-
-app.use("/budgets", budgetsRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on  ${process.env.SERVER_URL}:${port}`);
