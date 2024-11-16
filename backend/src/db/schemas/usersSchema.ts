ca8c10d0b9e40ae00f6496bd051b0b7470f83a90
sandhipsuriya <sandhipks@gmail.com>
2024-11-16 19:57:05 +0530

    Authenication Schema Updated with userName & password

--- backend/src/db/schemas/usersSchema.ts
+++ backend/src/db/schemas/usersSchema.ts
@@ -2,14 +2,12 @@ import { pgTable, serial, varchar, timestamp } from 'drizzle-orm/pg-core';
 import { createInsertSchema } from 'drizzle-zod';
 export const usersTable = pgTable('UsersTable', {
   userId: serial('user_id').primaryKey(),
-  firstName: varchar('first_name').notNull(),
-  lastName: varchar('last_name').notNull(),
-  email: varchar('email').notNull().unique(),
+  userName: varchar('user_name').notNull().unique(),
   password: varchar('password').notNull(),
   createdAt: timestamp('created_at').defaultNow(),
   role: varchar('role').default('user'),
 });
 
-export const loginSchema = createInsertSchema(usersTable).pick({ email: true, password: true });
+export const loginSchema = createInsertSchema(usersTable).pick({ userName: true, password: true });
 
 export const createUserSchema = createInsertSchema(usersTable).omit({ userId: true, createdAt: true, role: true });
