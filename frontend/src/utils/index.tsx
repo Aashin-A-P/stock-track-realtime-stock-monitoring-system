5326a3310e82dbfe51401fef07640c8df5762c3d
John-Prabu-A <johnprabu0702@gmail.com>
2025-02-16 09:37:43 +0530

    refactor: uploadImaageAndGetURL func extracted and added as utility func

--- frontend/src/utils/index.tsx
+++ frontend/src/utils/index.tsx
@@ -1,3 +1,5 @@
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 // Fetch data for locations, Statuses, and categories in parallel
 export const fetchMetadata = async (baseUrl: string, endpoint: string, key: string) =>
     await fetch(`${baseUrl}/${endpoint}?query=${key}`, {
@@ -7,3 +9,31 @@ export const fetchMetadata = async (baseUrl: string, endpoint: string, key: stri
         ? res.json()
         : Promise.reject(`Error fetching ${endpoint}: ${key}`)
     );
+
+  // Upload image and return URL
+  export const uploadImageAndGetURL = async (
+    file: File,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await fetch(`${baseURL}/upload`, {
+        method: "POST",
+        headers: {
+          Authorization: localStorage.getItem("token") || "",
+        },
+        body: formData,
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.error || "Error uploading image");
+      }
+      const details = await res.json();
+      return details.imageUrl;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      throw error;
+    }
+  };
